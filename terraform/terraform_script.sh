942941d010b83a30679fe244f55582c2e6790784
Joseph <jrichardson@jmstudios.net>
2017-06-10 14:01:26 -0700

    ansible is saving the terraform show to ansible hosts file not ex_hosts but with extra header

--- terraform/terraform_script.sh
+++ terraform/terraform_script.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
-#echo '[celery-server]' > ../ansible/hosts
+celery_header="[celery-server]"
+backend_server_header="[backend-server]"
+echo $celery_header > ../ansible/hosts
 
-#aws_url="ubuntu@"
+base_url="ubuntu@"
+targets=`terraform show |  grep public_dns | awk '{print $3}'` 
+urls=()
+for target in $targets
+do
+    url="$base_url$target"
+    echo $url >> ../ansible/hosts
+    echo $backend_server_header >> ../ansible/hosts
+done
+
+
 #url="$aws_url`terraform show | grep public_dns | awk '{print $3}'`"
 #echo $url > ../ansible/hosts
 
-ansible -i ../ansible/ex_hosts all -a "mkdir /opt/backend_api" --private-key=~/.ssh/latest_joseph_key --sudo
+#ansible -i ../ansible/ex_hosts all -a "mkdir /opt/backend_api" --private-key=~/.ssh/latest_joseph_key --sudo
