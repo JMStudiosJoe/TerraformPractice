3ef98f01c9d20a1a2991049313c39ec3bf047a3f
Joseph <jrichardson@jmstudios.net>
2017-06-13 16:43:01 -0700

    ran setup yml and got backend api onto servers

--- terraform/dns_config.tf
+++ terraform/dns_config.tf
@@ -1,25 +1,25 @@
-data "aws_route53_zone" "distribute" {
-  name = "distribute.com"
-}
+#data "aws_route53_zone" "distribute" {
+#  name = "distribute.com"
+#}
 
-data "aws_route53_zone" "distribute-int" {
-  name = "int.distribute.com"
-  vpc_id = "${data.aws_vpc.default.id}"
-}
+#data "aws_route53_zone" "distribute-int" {
+#  name = "int.distribute.com"
+#  vpc_id = "${data.aws_vpc.default.id}"
+#}
 
-resource "aws_route53_record" "BackendServer" {
-  zone_id = "${data.aws_route53_zone.distribute-int.zone_id}"
-  name = "ctrl.int.distribute.com"
-  type = "A"
-  ttl  = "300"
-  records = ["${aws_instance.ansible-ctrl.public_ip}"]
-}
+#resource "aws_route53_record" "BackendServer" {
+#  zone_id = "${data.aws_route53_zone.distribute-int.zone_id}"
+#  name = "ctrl.int.distribute.com"
+#  type = "A"
+#  ttl  = "300"
+#  records = ["${aws_instance.ansible-ctrl.public_ip}"]
+#}
 
-resource "aws_route53_record" "CeleryServer" {
-  zone_id = "${data.aws_route53_zone.distribute-int.zone_id}"
-  name = "load.int.distribute.com"
-  type = "A"
-  ttl  = "300"
-  records = ["${aws_instance.load.private_ip}"]
-}
+#resource "aws_route53_record" "CeleryServer" {
+#  zone_id = "${data.aws_route53_zone.distribute-int.zone_id}"
+#  name = "load.int.distribute.com"
+#  type = "A"
+#  ttl  = "300"
+#  records = ["${aws_instance.load.private_ip}"]
+#}
 
--- terraform/terraform_script.sh
+++ terraform/terraform_script.sh
@@ -13,6 +13,10 @@ do
     echo $backend_server_header >> ../ansible/hosts
 done
 terraform show | grep public_dns;cat ../ansible/hosts
+
+#ansible-playbook -i ../ansible/hosts ../ansible/playbooks/setup.yml --private-key=~/.ssh/latest_joseph_key --ssh-common-args='-o ForwardAgent=yes' --sudo
+#before can call git need to add ssh key to identity when passing ssh agent add the private key to agent by ssh-add ~/.ssh/key_to_add
+ssh-add ~/.ssh/latest_joseph_key
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/site.yml --private-key=~/.ssh/latest_joseph_key --ssh-common-args='-o ForwardAgent=yes'
 #url="$aws_url`terraform show | grep public_dns | awk '{print $3}'`"
 #echo $url > ../ansible/hosts
