067916fe14eaacbdcc202e1a2deeeef7c31eab03
Joseph <jrichardson@jmstudios.net>
2017-07-04 12:26:42 -0700

    now for cleanup for only deploying jmstudios

--- api.py
+++
@@ -1,8 +0,0 @@
-import pika
-from sqlalchemy.pool import QueuePool
-def main():
-    print("hello")
-
-
-if __name__ == '__main__':
-    main()
--- terraform/terraform_script.sh
+++ terraform/terraform_script.sh
@@ -19,7 +19,3 @@ ansible-playbook -i ../ansible/hosts ../ansible/playbooks/setup.yml --private-ke
 ssh-add ~/.ssh/latest_joseph_key
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/site.yml --private-key=~/.ssh/latest_joseph_key --ssh-common-args='-o ForwardAgent=yes'
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/project_setup.yml --private-key=~/.ssh/latest_joseph_key --ssh-common-args='-o ForwardAgent=yes'
-#url="$aws_url`terraform show | grep public_dns | awk '{print $3}'`"
-#echo $url > ../ansible/hosts
-#ssh -i ~/.ssh/latest_joseph_key ubuntu@ec2-52-38-206-242.us-west-2.compute.amazonaws.com
-#ansible -i ../ansible/ex_hosts all -a "mkdir /opt/backend_api" --private-key=~/.ssh/latest_joseph_key --sudo
