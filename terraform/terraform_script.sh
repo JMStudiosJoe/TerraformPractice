8b45a8553f538da3e5b4bd51bbdffa26e773d2e2
Joseph <jrichardson@jmstudios.net>
2017-09-16 15:22:17 -0700

    so far the terraform script will run without fail but flask still will not start but due to postgres and project setup not system

--- terraform/terraform_script.sh
+++ terraform/terraform_script.sh
@@ -18,4 +18,4 @@ ansible-playbook -i ../ansible/hosts ../ansible/playbooks/setup.yml --private-ke
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/site.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/project_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/backend_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
-ansible-playbook -i ../ansible/hosts ../ansible/playbooks/nginx_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
+ansible-playbook -i ../ansible/hosts ../ansible/playbooks/nginx_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes' --become
