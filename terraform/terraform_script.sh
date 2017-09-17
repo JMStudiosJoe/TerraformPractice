cca505d0c95f3373397705a13880941eb59cc468
Joseph <jrichardson@jmstudios.net>
2017-09-17 11:47:10 -0700

    almost there switching to first module a postgresql module

--- terraform/terraform_script.sh
+++ terraform/terraform_script.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
-#backend_server_header="[backend-server]"
+backend_server_header="[backend-server]"
 
-#base_url="ubuntu@"
-#targets=`terraform show |  grep public_dns | awk '{print $3}'` 
-#urls=()
-#for target in $targets
-#do
-#    url="$base_url$target"
-#    echo $backend_server_header >> ../ansible/hosts
-#    echo $url >> ../ansible/hosts
-#done
-#terraform show | grep public_dns;cat ../ansible/hosts
+base_url="ubuntu@"
+targets=`terraform show |  grep public_dns | awk '{print $3}'` 
+urls=()
+echo '' > ../ansible/hosts
+for target in $targets
+do
+    url="$base_url$target"
+    echo $backend_server_header >> ../ansible/hosts
+    echo $url >> ../ansible/hosts
+done
+terraform show | grep public_dns;cat ../ansible/hosts
 
 ssh-add ~/.ssh/latest_jrich_key
 
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes' --become
+ansible-playbook -i ../ansible/hosts ../ansible/playbooks/python_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes' --become
+ansible-playbook -i ../ansible/hosts ../ansible/playbooks/postgres_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes' --become-user=postgres
+
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/site.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/project_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/backend_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
