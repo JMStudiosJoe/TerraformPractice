5ec62f00d137bb4522acd64f4e1cc782d546c3f3
Joseph <jrichardson@jmstudios.net>
2017-09-17 13:56:44 -0700

    modules are better, less warnings and postgres is still having permission issues and creating users and db still off

--- terraform/terraform_script.sh
+++ terraform/terraform_script.sh
@@ -15,11 +15,11 @@ terraform show | grep public_dns;cat ../ansible/hosts
 
 ssh-add ~/.ssh/latest_jrich_key
 
-ansible-playbook -i ../ansible/hosts ../ansible/playbooks/setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes' --become
-ansible-playbook -i ../ansible/hosts ../ansible/playbooks/python_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes' --become
-ansible-playbook -i ../ansible/hosts ../ansible/playbooks/postgres_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes' --become-user=postgres
+ansible-playbook -i ../ansible/hosts ../ansible/playbooks/setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
+ansible-playbook -i ../ansible/hosts ../ansible/playbooks/python_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
+ansible-playbook -i ../ansible/hosts ../ansible/playbooks/postgres_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
 
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/site.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/project_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
 ansible-playbook -i ../ansible/hosts ../ansible/playbooks/backend_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
-ansible-playbook -i ../ansible/hosts ../ansible/playbooks/nginx_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes' --become
+ansible-playbook -i ../ansible/hosts ../ansible/playbooks/nginx_setup.yml --private-key=~/.ssh/latest_jrich_key --ssh-common-args='-o ForwardAgent=yes'
